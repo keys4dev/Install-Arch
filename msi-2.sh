63aedd4bdae241618de51155292713e30aecae3d
Keys4Dev <joseluis.sanchez.lopez@outlook.com>
2024-03-01 15:55:57 +0100

    Create msi-2.sh

---
+++ msi-2.sh
@@ -0,0 +1,17 @@
+ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+nano /etc/locale.gen
+locale-gen
+echo LANG=en_GB.UTF-8 > /etc/locale.conf
+export LANG=en_GB.UTF-8
+echo myarch > /etc/hostname
+nano /etc/hosts
+127.0.0.1	localhost
+::1		localhost
+127.0.1.1	myarch
+passwd
+pacman -S grub efibootmgr
+mkdir /boot/efi
+mount /dev/nvme0n1p1 /boot/efi
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S sudo
